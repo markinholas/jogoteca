f058ecd1032b59605a45eb48497c01dbc0dd4a49
markinholas <marcus_mafioso@hotmail.com>
2022-07-20 12:15:20 -0300

    jogoteca

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+app = Flask(__name__)
 class jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -12,7 +12,7 @@ jogo3 = jogo("Minecraft", "Construção", "PC")
 
 lista1 =  (jogo1, jogo2, jogo3)
 
-app = Flask(__name__)
+
 
 lista = ['God of War', 'LoL', 'Batman']
 
@@ -20,4 +20,7 @@ lista = ['God of War', 'LoL', 'Batman']
 def ola():
     return render_template('lista.html',titulo='Meus Jogos', jogos=lista1)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html')
 app.run()
